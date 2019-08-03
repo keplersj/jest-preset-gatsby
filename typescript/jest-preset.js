ffa6228b534e344f5b84f2093f83e02d3db407db
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-08-03 10:54:32 -0600

    refactor(typescript): use object spread to include base properties

--- typescript/jest-preset.js
+++ typescript/jest-preset.js
@@ -3,9 +3,9 @@ const base = require("../jest-preset");
 module.exports = {
   ...base,
   transform: {
-    "^.+\\.tsx?$": `${__dirname}/preprocess.js`,
-    "^.+\\.jsx?$": `${__dirname}/../preprocess.js`
+    ...base.transform,
+    "^.+\\.tsx?$": `${__dirname}/preprocess.js`
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", ...base.moduleFileExtensions]
 };
